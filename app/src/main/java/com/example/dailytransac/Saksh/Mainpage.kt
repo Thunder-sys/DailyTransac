d33c8df51ac31342d4a3523fffa45e92b135089d
kr115 <kr1158721@gmail.com>
2024-06-04 18:51:35 +0530

    I make a packages

--- app/src/main/java/com/example/dailytransac/Saksh/Mainpage.kt
+++ app/src/main/java/com/example/dailytransac/Saksh/Mainpage.kt
@@ -1,10 +1,11 @@
-package com.example.dailytransac
+package com.example.dailytransac.Saksh
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dailytransac.R
 
 class Mainpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/dailytransac/kuna/MainActivity.kt
+++ app/src/main/java/com/example/dailytransac/kuna/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.dailytransac
+package com.example.dailytransac.kuna
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dailytransac.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
