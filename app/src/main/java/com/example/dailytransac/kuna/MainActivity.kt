b85b42f26c565d5304fca611edc8fdb78c2e1306
admin <kunalwarrior25@gmail.com>
2024-06-05 18:31:35 +0530

    kunal

--- app/src/main/java/com/example/dailytransac/kuna/MainActivity.kt
+++ app/src/main/java/com/example/dailytransac/kuna/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         layout_list = findViewById(R.id.Layout_list)
         add_button = findViewById(R.id.add)
+
+
     }
 
 }
