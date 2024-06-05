c46335311dccf4817d000fff59ad4d836a2fdcc4
kunal <kunalwarrior25@gmail.com>
2024-06-05 12:33:56 +0530

    working on front activity_main.xml

--- app/src/main/java/com/example/dailytransac/kuna/MainActivity.kt
+++ app/src/main/java/com/example/dailytransac/kuna/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.dailytransac.kuna
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +11,15 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.dailytransac.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var layout_list:LinearLayout
+    lateinit var add_button:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+        layout_list = findViewById(R.id.Layout_list)
+        add_button = findViewById(R.id.add)
+
 }
