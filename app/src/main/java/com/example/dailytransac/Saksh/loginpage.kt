fd8e961e299d431546ba365f9eb744485eae8f53
lappy <shakshik732@gmail.com>
2024-06-18 18:56:50 +0530

    i add

--- app/src/main/java/com/example/dailytransac/Saksh/loginpage.kt
+++ app/src/main/java/com/example/dailytransac/Saksh/loginpage.kt
@@ -1,21 +1,25 @@
 package com.example.dailytransac.Saksh
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dailytransac.R
+import com.example.dailytransac.kuna.MainActivity
 
 class loginpage : AppCompatActivity() {
+    lateinit var log:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_loginpage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        log=findViewById(R.id.sign)
+        log.setOnClickListener(){
+            var intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/dailytransac/Saksh/loginpage2.kt
+++ app/src/main/java/com/example/dailytransac/Saksh/loginpage2.kt
@@ -1,21 +1,32 @@
 package com.example.dailytransac.Saksh
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dailytransac.R
+import com.example.dailytransac.kuna.MainActivity
 
 class loginpage2 : AppCompatActivity() {
+    lateinit var go:Button
+    lateinit var signup:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_loginpage2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+         go=findViewById(R.id.login)
+         signup=findViewById(R.id.create)
+        go.setOnClickListener(){
+            var jkl= Intent(this,MainActivity::class.java)
+            startActivity(jkl)
         }
+        signup.setOnClickListener(){
+            var jkl= Intent(this,loginpage::class.java)
+            startActivity(jkl)
+        }
+
     }
 }
