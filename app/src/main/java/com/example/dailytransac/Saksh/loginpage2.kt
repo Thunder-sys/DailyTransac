c84e5752899456dff6d11b28eb4ddcfceb34753b
lappy <shakshik732@gmail.com>
2024-06-28 19:16:18 +0530

    i add change

--- app/src/main/java/com/example/dailytransac/Saksh/loginpage2.kt
+++ app/src/main/java/com/example/dailytransac/Saksh/loginpage2.kt
@@ -8,19 +8,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.dailytransac.Database.User
 import com.example.dailytransac.R
 import com.example.dailytransac.databinding.ActivityLoginpage2Binding
 import com.example.dailytransac.databinding.ActivityLoginpageBinding
 import com.example.dailytransac.kuna.MainActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 
 class loginpage2 : AppCompatActivity() {
     lateinit var binding: ActivityLoginpage2Binding
     lateinit var firebaseAuth:FirebaseAuth
-    lateinit var database:DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -34,11 +30,7 @@ class loginpage2 : AppCompatActivity() {
 
             if (email.isNotEmpty() && pass.isNotEmpty())  {
                     firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
-
                         if (it.isSuccessful) {
-                            addUsertoDatabase(email,firebaseAuth.currentUser?.uid!!)
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
                         } else {
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show()
                         }
@@ -53,10 +45,10 @@ class loginpage2 : AppCompatActivity() {
             startActivity(jkl)
         }
     }
-
-    private fun addUsertoDatabase(email: String, uid: String) {
-        database = FirebaseDatabase.getInstance().getReference()
-        database.child("User").child(uid).setValue(User(email,uid))
+    override fun onStart() {
+        super.onStart()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
 
     }
 }
