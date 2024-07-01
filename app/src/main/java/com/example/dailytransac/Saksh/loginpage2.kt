8d1ecf75d0d6abc8d84bd39ad5e61c804932cf4a
lappy <shakshik732@gmail.com>
2024-07-01 08:41:03 +0530

    i add changes in signup page

--- app/src/main/java/com/example/dailytransac/Saksh/loginpage2.kt
+++ app/src/main/java/com/example/dailytransac/Saksh/loginpage2.kt
@@ -1,8 +1,12 @@
 package com.example.dailytransac.Saksh
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,39 +20,53 @@ import com.google.firebase.auth.FirebaseAuth
 
 class loginpage2 : AppCompatActivity() {
     lateinit var binding: ActivityLoginpage2Binding
-    lateinit var firebaseAuth:FirebaseAuth
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityLoginpage2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //text gradient
+        var txt = findViewById<TextView>(R.id.halo)
+        var pent = txt.paint
+        var widh = pent.measureText(txt.text.toString())
+        txt.paint.shader = LinearGradient(
+            0f, 0f, widh, txt.textSize, intArrayOf(
+                Color.parseColor("#a6767c"),
+                Color.parseColor("#cf768b"),
+                Color.parseColor("#9c2542"),
+                Color.parseColor("#f892a9"),
+
+                ), null, Shader.TileMode.REPEAT
+        )
+
+
+
+
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.login.setOnClickListener(){
+        binding.login.setOnClickListener() {
             val email = binding.reEmail.text.toString()
             val pass = binding.rePassword.text.toString()
 
-            if (email.isNotEmpty() && pass.isNotEmpty())  {
-                    firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                        } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show()
-                        }
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show()
                     }
                 }
-                else {
+            } else {
                 Toast.makeText(this, "Password is not mataching", Toast.LENGTH_LONG).show()
             }
         }
-        binding.create.setOnClickListener(){
-            var jkl= Intent(this,loginpage::class.java)
+        binding.create.setOnClickListener() {
+            var jkl = Intent(this, loginpage::class.java)
             startActivity(jkl)
         }
-    }
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-
     }
 }
