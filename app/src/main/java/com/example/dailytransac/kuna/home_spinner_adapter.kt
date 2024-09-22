f4e574bec8a192537cbee5b59118becce1f3efbf
kunal <kunalwarrior25@gmail.com>
2024-09-22 15:31:11 +0530

    complete

--- app/src/main/java/com/example/dailytransac/kuna/home_spinner_adapter.kt
+++ app/src/main/java/com/example/dailytransac/kuna/home_spinner_adapter.kt
@@ -3,15 +3,14 @@ package com.example.dailytransac.kuna
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dailytransac.R
+import kotlinx.coroutines.withContext
 
 class home_spinner_adapter(var item:ArrayList<home_spinner_model>, private val onItemClick: (home_spinner_model) -> Unit):RecyclerView.Adapter<home_spinner_adapter.viewholder>() {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): home_spinner_adapter.viewholder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): home_spinner_adapter.viewholder {
         var view = LayoutInflater.from(parent.context).inflate(R.layout.graph_spinner_recycleview,parent,false)
         return viewholder(view)
     }
